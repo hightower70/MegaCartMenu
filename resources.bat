d55f03ea8cd18587a487f074fa29c5a94f39845b
László Árvai <laszlo.arvai@gmail.com>
2023-03-05 18:34:22 +0100

    Music Player changed to PSG player

---
+++ resources.bat
@@ -0,0 +1,4 @@
+sjasmplus.exe --raw=res2019.bin  res2019.a80
+sjasmplus.exe --raw=res2020.bin  res2020.a80
+sjasmplus.exe --raw=res2021.bin  res2021.a80
+sjasmplus.exe --raw=res2022.bin  res2022.a80
